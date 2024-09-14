70411368e617b98f954601390612a0ca39c006a9
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2024-09-14 09:43:46 -0400

    Add project files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import VideoCapture from './VideoCapture'; // Import the component
+
+function App() {
+    return (
+        <div className="App">
+            <VideoCapture />
+        </div>
+    );
+}
+
+export default App;
