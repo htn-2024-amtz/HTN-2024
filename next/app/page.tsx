8cd8040f9a05e5d64a74417753cd75c06c49f0f6
timkn <timknothe21@gmail.com>
2024-09-14 13:16:15 -0400

    host test

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "flowbite-react";
 import VideoCapture from "@/app/components/video-capture";
 
+
+//       <VideoCapture />
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Button>Click me</Button>
-      <VideoCapture />
     </div>
   );
 }
